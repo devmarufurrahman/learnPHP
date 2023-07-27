e61f8f25f1de4f281508f7abb118da286fc74c6b
maruf5682 <mdmarufur5682@gmail.com>
2023-07-27 15:35:45 +0600

    operator done

---
+++ learn3/arithmaticOperator.php
@@ -0,0 +1,21 @@
+<?php
+
+    $a = 10;
+    $b = 45;
+
+
+    $c = $a + $b;
+
+    echo $c;
+
+
+    // Addition +
+    // Subtraction - 
+    // Multiplication *
+    // Exponentiation **    // power
+    // Division  /
+    // Modulus  %
+    // Increment  ++
+    // Decrement  --
+
+?>
---
+++ learn3/assignmentOperator.php
@@ -0,0 +1,21 @@
+<?php
+/*
+PHP Assignment Operators
+The PHP assignment operators are used with numeric values to write a value to a variable.
+
+The basic assignment operator in PHP is "=". It means that the left operand gets set to the value of the assignment expression on the right.
+
+Assignment	Same as 
+x = y	x = y	    The left operand gets set to the value of the expression on the right	
+x += y	x = x + y	Addition	
+x -= y	x = x - y	Subtraction	
+x *= y	x = x * y	Multiplication	
+x /= y	x = x / y	Division	
+x %= y	x = x % y	Modulus	
+
+
+*/
+
+
+
+?>
