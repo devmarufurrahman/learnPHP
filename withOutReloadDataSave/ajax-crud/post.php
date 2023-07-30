03d57b450d75d0e76cfbfb54aa47c2e5ce3e8937
maruf5682 <mdmarufur5682@gmail.com>
2023-07-30 13:21:13 +0600

    Edit and Update Done

--- withOutReloadDataSave/ajax-crud/post.php
+++ withOutReloadDataSave/ajax-crud/post.php
@@ -2,6 +2,8 @@
 
     $conn = mysqli_connect("localhost","root","","ajaxCrud");
 
+
+    // Data input in Database 
     if(isset($_POST['isData'])){
         $name = $_POST['Name'];
         $email = $_POST['Email'];
@@ -12,7 +14,7 @@
     }
     
 
-
+    // Data view from Database with id 
     if(isset($_POST['isView'])){
         $dataId = $_POST['dataId'];
 
@@ -35,5 +37,42 @@
         }
     }
 
+
+
+
+    // Data edit from Database with id 
+    if(isset($_POST['isEdit'])){
+        $dataId = $_POST['dataId'];
+
+
+        $query = "SELECT * FROM Data WHERE id=$dataId";
+        $query_run= mysqli_query($conn,$query);
+
+        $result_array = [];
+
+
+        if(mysqli_num_rows($query_run) > 0){
+            foreach($query_run as $row){
+                array_push($result_array,$row); 
+            }
+    
+            header('Content-type: application/json');
+            echo json_encode($result_array); 
+        } else{
+            echo "<h4>No Record Found!</h4>";
+        }
+    }
+
+
+    // Data input in Database 
+    if(isset($_POST['isUpdate'])){
+        $id = $_POST['id'];
+        $name = $_POST['Name'];
+        $email = $_POST['Email'];
+
+
+        $query = "UPDATE Data SET Name='$name',Email='$email' WHERE id=$id";
+        $query_run = mysqli_query($conn,$query);
+    }
 
 ?>
